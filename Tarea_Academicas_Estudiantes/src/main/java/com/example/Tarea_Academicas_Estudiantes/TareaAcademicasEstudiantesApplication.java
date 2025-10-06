03757b98c813b0a30d3c3e7e285e4076397596db
luism7p7 <luism.munoz@campusucc.edu.co>
2025-10-06 08:57:44 -0500

    base Spreen Boot

---
+++ Tarea_Academicas_Estudiantes/src/main/java/com/example/Tarea_Academicas_Estudiantes/TareaAcademicasEstudiantesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tarea_Academicas_Estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaAcademicasEstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaAcademicasEstudiantesApplication.class, args);
+	}
+
+}
---
+++ Tarea_Academicas_Estudiantes/src/test/java/com/example/Tarea_Academicas_Estudiantes/TareaAcademicasEstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tarea_Academicas_Estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaAcademicasEstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
